de3af407d5703e922cffbba194f5861c05365cca
simonmetcalfe <simon.metcalfe@gmail.com>
2025-09-21 20:46:40 +0200

    Revert sensor hub back to original settings

--- components/empty_sensor_hub/empty_sensor_hub.cpp
+++ components/empty_sensor_hub/empty_sensor_hub.cpp
@@ -1,49 +1,28 @@
 #include "empty_sensor_hub.h"
 #include "esphome/core/log.h"
 
-namespace esphome
-{
-    namespace empty_sensor_hub
-    {
+namespace esphome {
+namespace empty_sensor_hub {
 
-        static const char *TAG = "empty_sensor_hub.component";
+static const char *TAG = "empty_sensor_hub.component";
 
-        void EmptySensorHub::setup()
-        {
-        }
+void EmptySensorHub::setup(){
 
-        void EmptySensorHub::dump_config()
-        {
-            for (auto *sensor : this->sensor1_)
-            {
-                LOG_SENSOR("  ", "Plant 1", sensor);
-            }
+}
 
-            for (auto *sensor : this->sensor2_)
-            {
-                LOG_SENSOR("  ", "Plant 2", sensor);
-            }
+void EmptySensorHub::dump_config(){
+    for (auto *sensor : this->sensors_) {
+        LOG_SENSOR("  ", "Sensor", sensor);
+    }
 
-            for (auto *sensor : this->sensor3_)
-            {
-                LOG_SENSOR("  ", "Plant 3", sensor);
-            }
+    for(auto *text_sensor : this->text_sensors_){
+        LOG_TEXT_SENSOR("  ", "Text sensor", text_sensor);
+    }
 
-            for (auto *sensor : this->sensor4_)
-            {
-                LOG_SENSOR("  ", "Plant 4", sensor);
-            }
+    for(auto *binary_sensor : this->binary_sensors_){
+        LOG_BINARY_SENSOR("  ", "Binary sensor", binary_sensor);
+    }
+}
 
-            for (auto *text_sensor : this->text_sensors_)
-            {
-                LOG_TEXT_SENSOR("  ", "Status", text_sensor);
-            }
-
-            for (auto *binary_sensor : this->binary_sensors_)
-            {
-                LOG_BINARY_SENSOR("  ", "Tank", binary_sensor);
-            }
-        }
-
-    } // namespace empty_sensor_hub
-} // namespace esphome
+} //namespace empty_sensor_hub
+} //namespace esphome
