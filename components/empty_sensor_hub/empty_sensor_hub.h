1bed72b24dc9474676c8d07bd426aa2f0f14fb45
simonmetcalfe <simon.metcalfe@gmail.com>
2025-09-21 19:07:54 +0200

    Update

--- components/empty_sensor_hub/empty_sensor_hub.h
+++ components/empty_sensor_hub/empty_sensor_hub.h
@@ -5,22 +5,31 @@
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
-namespace esphome {
-namespace empty_sensor_hub {
+namespace esphome
+{
+  namespace empty_sensor_hub
+  {
 
-class EmptySensorHub : public Component {
- public:
-  void register_sensor(sensor::Sensor *obj) { this->sensors_.push_back(obj); }
-  void register_text_sensor(text_sensor::TextSensor *obj) { this->text_sensors_.push_back(obj); }
-  void register_binary_sensor(binary_sensor::BinarySensor *obj) { this->binary_sensors_.push_back(obj); }
-  void setup() override;
-  void dump_config() override;
-  
- protected:
-  std::vector<sensor::Sensor *> sensors_;
-  std::vector<text_sensor::TextSensor *> text_sensors_;
-  std::vector<binary_sensor::BinarySensor *> binary_sensors_;
-};
+    class EmptySensorHub : public Component
+    {
+    public:
+      void register_sensor(sensor::Sensor *obj) { this->sensor1_.push_back(obj); }
+      void register_sensor(sensor::Sensor *obj) { this->sensor2_.push_back(obj); }
+      void register_sensor(sensor::Sensor *obj) { this->sensor3_.push_back(obj); }
+      void register_sensor(sensor::Sensor *obj) { this->sensor4_.push_back(obj); }
+      void register_text_sensor(text_sensor::TextSensor *obj) { this->text_sensors_.push_back(obj); }
+      void register_binary_sensor(binary_sensor::BinarySensor *obj) { this->binary_sensors_.push_back(obj); }
+      void setup() override;
+      void dump_config() override;
 
-} //namespace empty_sensor_hub
-} //namespace esphome
+    protected:
+      std::vector<sensor::Sensor *> sensor1_;
+      std::vector<sensor::Sensor *> sensor2_;
+      std::vector<sensor::Sensor *> sensor3_;
+      std::vector<sensor::Sensor *> sensor4_;
+      std::vector<text_sensor::TextSensor *> text_sensors_;
+      std::vector<binary_sensor::BinarySensor *> binary_sensors_;
+    };
+
+  } // namespace empty_sensor_hub
+} // namespace esphome
