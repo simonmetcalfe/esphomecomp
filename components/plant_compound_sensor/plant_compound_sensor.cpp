2938a300dbf7a43548bbe0a6b412e9f2b1673c25
simonmetcalfe <simon.metcalfe@gmail.com>
2026-01-17 10:45:03 +0100

    Add project yaml + change values
    Testing of cache

--- components/plant_compound_sensor/plant_compound_sensor.cpp
+++ components/plant_compound_sensor/plant_compound_sensor.cpp
@@ -19,13 +19,13 @@ namespace esphome
     void PlantCompoundSensor::update()
     {
       if (this->cplant1_ != nullptr)
-        this->cplant1_->publish_state(1.0f);
+        this->cplant1_->publish_state(5.0f);
       if (this->cplant2_ != nullptr)
-        this->cplant2_->publish_state(2.0f);
+        this->cplant2_->publish_state(6.0f);
       if (this->cplant3_ != nullptr)
-        this->cplant3_->publish_state(3.0f);
+        this->cplant3_->publish_state(7.0f);
       if (this->cplant4_ != nullptr)
-        this->cplant4_->publish_state(4.0f);
+        this->cplant4_->publish_state(8.0f);
     }
 
     void PlantCompoundSensor::dump_config()
